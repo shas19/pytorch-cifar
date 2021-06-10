2c80b4bdf0c8b21749efc0f50638eb417d73cff1
shas19 <shubhamdugare@gmail.com>
2021-06-10 12:34:52 -0500

    quantizing vgg16

---
+++ quantize.py
@@ -0,0 +1,83 @@
+'''Post training quantization of VGG16.'''
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+
+import torchvision
+import torchvision.transforms as transforms
+
+import os
+
+from models import *
+from utils import progress_bar
+
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# Data
+print('==> Preparing data..')
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+testset = torchvision.datasets.CIFAR10(
+    root='./data', train=False, download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(
+    testset, batch_size=100, shuffle=False, num_workers=2)
+
+# classes = ('plane', 'car', 'bird', 'cat', 'deer',
+#            'dog', 'frog', 'horse', 'ship', 'truck')
+
+# Model
+print('==> Building model..')
+net = VGGQ('VGG16')
+
+net = net.to(device)
+if device == 'cuda':
+    net = torch.nn.DataParallel(net)
+    cudnn.benchmark = True
+
+# if args.resume:
+    # Load checkpoint.
+print('==> Resuming from checkpoint..')
+assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+checkpoint = torch.load('./checkpoint/ckpt.pth')
+net.load_state_dict(checkpoint['net'])
+start_epoch = checkpoint['epoch']
+
+net.eval()
+
+net.qconfig = torch.quantization.get_default_qconfig('fbgemm')
+
+net_fp32_prepared = torch.quantization.prepare(net)
+input_fp32 = torch.randn(100, 3, 32, 32)
+net_fp32_prepared(input_fp32)
+
+net_int8 = torch.quantization.convert(net_fp32_prepared)
+
+
+def test():
+    net_int8.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            # print(inputs.shape)
+            outputs = net_int8(inputs)
+
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            progress_bar(batch_idx, len(testloader), 'Acc: %.3f%% (%d/%d)'
+                         % (100.*correct/total, correct, total))
+
+
+# the main code
+test()
